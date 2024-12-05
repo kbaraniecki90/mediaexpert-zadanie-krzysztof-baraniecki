fc01c07bbf898cf3b74674f5e12ffbb4e587ba02
Krzysiek-x1 <kbaraniecki@wp.pl>
2024-12-05 16:20:33 +0100

    remove old components in app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
 import Panel from "./components/Panel.vue";
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
 </script>
 
 <template>
