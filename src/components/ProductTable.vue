ca08ae595f114a0190c5a99f7106ab960483cd80
Krzysiek-x1 <kbaraniecki@wp.pl>
2024-12-05 18:55:06 +0100

    add product Table component

---
+++ src/components/ProductTable.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>Lista produktów</h2>
+    <table class="table mt-3">
+      <thead>
+        <tr>
+          <th>Nazwa produktu</th>
+          <th>Ilość</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(product, index) in products" :key="index">
+          <td>{{ product.name }}</td>
+          <td>{{ product.quantity }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+export default defineComponent({
+  props: {
+    products: {
+      type: Array as PropType<{ name: string; quantity: number }[]>,
+      required: true,
+    },
+  },
+});
+</script>
