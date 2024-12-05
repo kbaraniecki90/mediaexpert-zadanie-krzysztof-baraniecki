1722591125d44df2cf507be7c9352d183fff1954
Krzysiek-x1 <kbaraniecki@wp.pl>
2024-12-05 18:58:20 +0100

    use ProductTable in panel with example data

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -1,9 +1,36 @@
-<script setup lang="ts">
-import AddProduct from "./AddProduct.vue";
-</script>
-
 <template>
   <section class="container mt-5">
-    <AddProduct />
+    <AddProduct @addProduct="handleAddProduct" />
+  </section>
+
+  <section class="container mt-4">
+    <ProductTable :products="products" />
   </section>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import AddProduct from "./AddProduct.vue";
+import ProductTable from "./ProductTable.vue";
+
+export default defineComponent({
+  components: {
+    AddProduct,
+    ProductTable,
+  },
+  data() {
+    return {
+      products: [
+        { name: "Produkt 1", quantity: 1 },
+        { name: "Produkt 2", quantity: 3 },
+        { name: "Produkt 3", quantity: 2 },
+      ],
+    };
+  },
+  methods: {
+    handleAddProduct(product: { name: string; quantity: number }) {
+      this.products.push(product);
+    },
+  },
+});
+</script>
